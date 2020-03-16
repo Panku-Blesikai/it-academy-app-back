3c42527aeb6717b9ede63f66373b5eb23ea730b4
kotryna <kotryna.slepetyte@gmail.com>
2020-03-16 19:07:31 +0200

    added post and name validator

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
-import com.mongodb.DBObject;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
+
+import com.example.demo.validator.ApplicationFormValidator;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class DemoController {
@@ -16,24 +14,17 @@ public class DemoController {
     @Autowired
     DemoRepository repo;
 
-//    @GetMapping(value = "/")
-//    public String helloWorld() {
-//        StringBuilder result = new StringBuilder(BE_SUCCESS_MESSAGE);
-//
-//        List<ApplicationForm> applicationForms = repo.findAll();
-//        Optional<ApplicationForm> message = applicationForms.stream().findFirst();
-//        message.ifPresent(value -> result.append(value.getMessage()));
-//
-//        return result.toString();
-//    }
-
     @GetMapping(value = "/getAll")
     public List<ApplicationForm> getAllApplications() {
         return repo.findAll();
     }
 
-    @GetMapping(value = "/add")
-    public ApplicationForm add() {
-        return repo.save(new ApplicationForm("email", "edu", "a", "email", "edu", "test", "test", "test", "test", "test"));
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public ApplicationForm addApplication(@RequestBody ApplicationForm applicationForm) {
+        ApplicationFormValidator validator = new ApplicationFormValidator();
+        validator.validate(applicationForm);
+        applicationForm.setId(ObjectId.get());
+        return repo.save(applicationForm);
     }
 }
