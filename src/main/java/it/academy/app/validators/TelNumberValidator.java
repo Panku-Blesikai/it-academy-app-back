29214c944556e72917f34bb9cf1a0e6e16528add
Kotryna Šlepetytė <49883485+sniegapele@users.noreply.github.com>
2020-03-24 00:20:00 +0200

    fixed regex in telephone number validator

--- src/main/java/it/academy/app/validators/TelNumberValidator.java
+++ src/main/java/it/academy/app/validators/TelNumberValidator.java
@@ -16,7 +16,7 @@ public class TelNumberValidator extends Validator<String> {
     }
 
     private void checkNumberByNumberFormat(String number) {
-        String phoneNumberRegex = "^([+370]+[0-9]+[-]+[0-9]{2}+[-]+[0-9]{5})$";
+        String phoneNumberRegex = "^+370+[0-9]+\-+[0-9]{2}+\-+[0-9]{5}$";
         System.out.println(number);
         Pattern pat = Pattern.compile(phoneNumberRegex);
 
