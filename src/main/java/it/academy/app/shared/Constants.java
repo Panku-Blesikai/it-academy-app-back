673133a33085333350fad2ce73af9589abc3c3bb
Arsenij <arsendk@gmail.com>
2020-04-03 14:13:08 +0300

    code cleaning

--- src/main/java/it/academy/app/shared/Constants.java
+++ src/main/java/it/academy/app/shared/Constants.java
@@ -6,10 +6,10 @@ public final class Constants {
 
     }
 
-    public static final String DB_URI = "MONGODB_URI";
-    public static final String DB_NAME = "MONGODB_NAME";
-    public static final String COLLECTION_ADMIN = "COLLECTION_ADMIN";
-    public static final String COLLECTION_APPLICATIONFORM = "COLLECTION_APPLICATIONFORM";
+    public static final String DB_URI = System.getenv("MONGODB_URI");
+    public static final String DB_NAME =  System.getenv("MONGODB_NAME");
+    public static final String COLLECTION_ADMIN = System.getenv("COLLECTION_ADMIN");
+    public static final String COLLECTION_APPLICATIONFORM = System.getenv("COLLECTION_APPLICATIONFORM");
     public static final String EMAIL = "pankublesikai@gmail.com";
     public static final int LOG_ROUNDS = 12;
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
